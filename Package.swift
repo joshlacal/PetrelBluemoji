cdeb84be1ab8a7de86cd04686fe1f26d0b47104c
Josh LaCalamito <joshlacal@gmail.com>
2026-07-18 03:48:34 -0400

    Use remote Petrel dependency instead of local path

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   name: "PetrelBluemoji",
   platforms: [.iOS(.v18), .macOS(.v15)],
   products: [.library(name: "PetrelBluemoji", targets: ["PetrelBluemoji"])],
-  dependencies: [.package(path: "../Petrel")],
+  dependencies: [.package(url: "https://github.com/joshlacal/Petrel.git", from: "1.0.1")],
   targets: [
     .target(
       name: "PetrelBluemoji",
